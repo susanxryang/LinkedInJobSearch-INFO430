24834aee677b1ad15c9afc1e7128b9dbe4a1d0db
xuanry <xuanry@uw.edu>
2022-06-01 11:34:13 -0700

    add view to cte

--- complexQueries.sql
+++ complexQueries.sql
@@ -21,12 +21,25 @@ WHERE RankingPosting <= 10
 ORDER BY RankingPosting
 GO
 
+CREATE VIEW TOP_IND_POSTING AS (
+        SELECT I.IndustryID, I.IndustryName,
+            RANK() OVER (ORDER BY COUNT(J.JobID) DESC) AS RankingPosting
+        FROM tblIndustry I
+            JOIN tblEmployer E ON I.IndustryID = E.IndustryID
+            JOIN tblJob J ON E.EmployerID = J.EmployerID
+            JOIN tblJobStatus JS ON JS.JobID = J.JobID
+            JOIN tblStatus S ON S.StatusID = JS.StatusID
+        WHERE S.StatusName = 'Open'
+        GROUP BY I.IndustryID, I.IndustryName
+    )
+GO
+
 -- Complex query: rank the companies with the highest average pay
 WITH
     CTE_TOP_EMP_SAL(EmployerID, EmployerName, AvgSalary, RankingSalary)
     AS
     (
-        SELECT E.EmployerID, E.EmployerName, SUM(J.Salary)/COUNT(J.JobID),
+        SELECT E.EmployerID, E.EmployerName, SUM(J.Salary)/COUNT(J.JobID) AS AvgSalary,
             RANK() OVER (ORDER BY SUM(J.Salary)/COUNT(J.JobID) DESC) AS RankingPosting
         FROM tblEmployer E
             JOIN tblJob J ON E.EmployerID = J.EmployerID
@@ -36,3 +49,14 @@ SELECT EmployerID, EmployerName, AvgSalary, RankingSalary
 FROM CTE_TOP_EMP_SAL
 ORDER BY RankingSalary
 GO
+
+CREATE VIEW TOP_EMP_SAL AS (
+        SELECT E.EmployerID, E.EmployerName, SUM(J.Salary)/COUNT(J.JobID) AS AvgSalary,
+            RANK() OVER (ORDER BY SUM(J.Salary)/COUNT(J.JobID) DESC) AS RankingPosting
+        FROM tblEmployer E
+            JOIN tblJob J ON E.EmployerID = J.EmployerID
+        GROUP BY E.EmployerID, E.EmployerName
+    )
+GO
+
+-- SELECT * FROM TOP_EMP_SAL
